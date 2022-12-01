1fc87a1af6132c0bf8e0d8f938f90c8f82bf89c8
Tim La <tlamarketing111@gmail.com>
2022-12-01 08:18:37 -0500

    logic for titleText made

--- starter_repo/sections/Explore.jsx
+++ starter_repo/sections/Explore.jsx
@@ -9,8 +9,19 @@ import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
 
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| The World" textStyles="text-center" />
+      <TitleText
+        title={<>Choose the world you want <br className="md:block hidden" /> to explore</>}
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
