230839df3cf845943d0a1254a8b204ed8dedf6e0
Tim La <tlamarketing111@gmail.com>
2022-11-30 19:19:56 -0500

    navbar completed

--- starter_repo/components/Navbar.jsx
+++ starter_repo/components/Navbar.jsx
@@ -21,6 +21,14 @@ const Navbar = () => (
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
 
   </motion.nav>
