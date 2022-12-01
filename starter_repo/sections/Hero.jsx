7d680e77b2b313e55315a1b351638ad16add86b4
Tim La <tlamarketing111@gmail.com>
2022-11-30 19:24:23 -0500

    hero heading displaying

--- starter_repo/sections/Hero.jsx
+++ starter_repo/sections/Hero.jsx
@@ -14,7 +14,19 @@ const Hero = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      Hi
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+      </div>
     </motion.div>
   </section>
 );
