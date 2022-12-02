0bf53afa6b6df2838012feabfe7be8876c34440a
Tim La <tlamarketing111@gmail.com>
2022-12-02 07:53:17 -0500

    mapped through newFeaturees

--- starter_repo/sections/WhatsNew.jsx
+++ starter_repo/sections/WhatsNew.jsx
@@ -20,7 +20,13 @@ const WhatsNew = () => (
         variants={fadeIn('right', 'tween', 0.2, 1)}
         className="flex-[0.95] flex justify-center flex-col"
       >
-        <TypingText />
+        <TypingText title="| Whats new?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
       </motion.div>
     </motion.div>
   </section>
