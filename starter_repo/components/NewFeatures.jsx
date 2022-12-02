0560d0dd805a2eb0640a65f3f21bdc34988d5811
Tim La <tlamarketing111@gmail.com>
2022-12-02 07:54:59 -0500

    building layout for NewFeatures

--- starter_repo/components/NewFeatures.jsx
+++ starter_repo/components/NewFeatures.jsx
@@ -1,6 +1,10 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}>
+      h
+    </div>
   </div>
 );
 
