9bb8b21bcf3781ab1bcd2598c1b9c033de4b03dd
Tim La <tlamarketing111@gmail.com>
2022-12-02 07:44:22 -0500

    image transitions in

--- starter_repo/sections/GetStarted.jsx
+++ starter_repo/sections/GetStarted.jsx
@@ -8,8 +8,27 @@ import { StartSteps, TitleText, TypingText } from '../components';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
+
   </section>
 );
 
