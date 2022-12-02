cbf719190558bc74de34873467bcf9ff7e002c3d
Tim La <tlamarketing111@gmail.com>
2022-12-02 07:47:28 -0500

    styling StartSteps

--- starter_repo/components/StartSteps.jsx
+++ starter_repo/components/StartSteps.jsx
@@ -1,5 +1,7 @@
-const StartSteps = () => (
-  <div>
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
     start steps
   </div>
 );
