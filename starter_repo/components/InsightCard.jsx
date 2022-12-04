98df24e5a0d357fd9b0a572d72520d9944d490f8
Tim La <tlamarketing111@gmail.com>
2022-12-04 08:29:58 -0500

    insight cards appearing

--- starter_repo/components/InsightCard.jsx
+++ starter_repo/components/InsightCard.jsx
@@ -14,6 +14,16 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       alt="planet-01"
       className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
     />
+    <div className="w-full flex justify-between items-center">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white">
+          {title}
+        </h4>
+        <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
+          {subtitle}
+        </p>
+      </div>
+    </div>
   </motion.div>
 );
 
