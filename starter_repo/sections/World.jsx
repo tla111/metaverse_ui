b19c727fec6ea6d44d453e8d89a40c5d98bfb13a
Tim La <tlamarketing111@gmail.com>
2022-12-04 08:22:56 -0500

    map of world displaying

--- starter_repo/sections/World.jsx
+++ starter_repo/sections/World.jsx
@@ -24,6 +24,13 @@ const World = () => (
         )}
         textStyles="text-center"
       />
+
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+      </motion.div>
     </motion.div>
   </section>
 );
