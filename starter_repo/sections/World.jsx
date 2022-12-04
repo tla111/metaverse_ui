8aa60a98f3bc7b785ddf255eede45fbcb16c13d5
Tim La <tlamarketing111@gmail.com>
2022-12-04 08:22:05 -0500

    building layout for world

--- starter_repo/sections/World.jsx
+++ starter_repo/sections/World.jsx
@@ -7,8 +7,24 @@ import { TitleText, TypingText } from '../components';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        title={(
+          <>Track friends around you and invite them to play together in the same
+            world
+          </>
+        )}
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
