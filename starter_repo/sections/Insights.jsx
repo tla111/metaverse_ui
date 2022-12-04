3eeaff34cb4317f5c009b79cf7a66de455ecc939
Tim La <tlamarketing111@gmail.com>
2022-12-04 08:25:31 -0500

    imported imports for insights

--- starter_repo/sections/Insights.jsx
+++ starter_repo/sections/Insights.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { insights } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+
+
 const Insights = () => (
   <section>
     Insights section
