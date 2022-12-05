014982ebd004900124023fcb84b1bb46f6a9cfb7
Tim La <tlamarketing111@gmail.com>
2022-12-05 08:05:22 -0500

    building layout for feedback

--- starter_repo/sections/Feedback.jsx
+++ starter_repo/sections/Feedback.jsx
@@ -1,8 +1,26 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+      >
+
+      </motion.div>
+    </motion.div>
   </section>
 );
 
