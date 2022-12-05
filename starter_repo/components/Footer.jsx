e85b2b7003d277b251b4f5bc88f1e799713aafdd
Tim La <tlamarketing111@gmail.com>
2022-12-05 08:11:10 -0500

    building layout for footer

--- starter_repo/components/Footer.jsx
+++ starter_repo/components/Footer.jsx
@@ -7,8 +7,20 @@ import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
 const Footer = () => (
-  <motion.div>
-    footer
+  <motion.div
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the Metaverse
+        </h4>
+      </div>
+    </div>
   </motion.div>
 );
 
