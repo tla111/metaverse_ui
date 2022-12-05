0e57164665cbabf1c66337e8f7cad66d11286f6b
Tim La <tlamarketing111@gmail.com>
2022-12-05 08:10:05 -0500

    imported imports for footer

--- starter_repo/components/Footer.jsx
+++ starter_repo/components/Footer.jsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
 const Footer = () => (
-  <footer>
+  <motion.div>
     footer
-  </footer>
+  </motion.div>
 );
 
 export default Footer;
