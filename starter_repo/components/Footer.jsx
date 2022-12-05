096b9e6dcbf7defadba9beca10e7d17fbbf28b81
Tim La <tlamarketing111@gmail.com>
2022-12-05 08:17:10 -0500

    completed project

--- starter_repo/components/Footer.jsx
+++ starter_repo/components/Footer.jsx
@@ -41,6 +41,16 @@ const Footer = () => (
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
           </p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
